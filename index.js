61885fdd081b0ff86bfe5d167f17c2716ed72f85
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-03 13:08:13 -0600

    commit tree 2eca2d6561b12fd69cac51e695b247f5d36c9e86

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joosh",
+    streetAdress: "1 beast ave",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ let newObject = {...employee};
+ newObject[key] = value;
+ return newObject;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
